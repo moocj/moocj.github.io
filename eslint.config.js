57f35f725d2496874f36e792b0578b2d8e7bb345
Jack Moocarme <139785504+moocj@users.noreply.github.com>
2026-09-20 17:18:23 +0100

    chore: add eslint and prettier and scripts for them

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist/"]),
+
+  js.configs.recommended,
+
+  {
+    files: ["src/**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+    },
+  },
+
+  {
+    files: ["*.js"],
+    languageOptions: { globals: globals.node },
+  },
+
+
+  prettier,
+]);
